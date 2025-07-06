99f1ea976252cfc8288e6966a27bc4199beec9b5
Pham Bach An Duy <phambachanduy@gmail.com>
2025-07-06 16:45:46 +0700

    [UPDATE] Upload specialties images && loading data specialties

--- src/containers/System/Specialty/ManageSpecialty.js
+++ src/containers/System/Specialty/ManageSpecialty.js
@@ -18,7 +18,6 @@ class ManageSpecialty extends Component {
             imageBase64: '',
             descriptionHTML: '',
             descriptionMarkdown: '',
-
         }
     }
 
@@ -63,6 +62,12 @@ class ManageSpecialty extends Component {
         let res = await createNewSpecialty(this.state);
         if (res && res.errCode === 0) {
             toast.success('Add new specialty succeed!')
+            this.setState({
+                name: '',
+                imageBase64: '',
+                descriptionHTML: '',
+                descriptionMarkdown: '',
+            })
         } else {
             toast.error("Something wrongs...")
         }
