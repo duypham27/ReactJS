08c756ccb10ad3d9e2ad899ef978d373c6593b5d
Pham Bach An Duy <phambachanduy@gmail.com>
2025-07-11 12:45:34 +0700

    [UPDATE] Finished design show detail clinic page

--- src/containers/System/Admin/ManageDoctor.js
+++ src/containers/System/Admin/ManageDoctor.js
@@ -95,6 +95,17 @@ class ManageDoctor extends Component {
                     result.push(object);
                 })
             }
+
+            if (type === 'CLINIC') {
+                inputData.map((item, index) => {
+                    let object = {};
+
+                    object.label = item.name;
+                    object.value = item.id;
+                    result.push(object);
+                })
+            }
+
         }
         return result;
     }
@@ -108,18 +119,20 @@ class ManageDoctor extends Component {
         }
 
         if (prevProps.allRequiredDoctorInfor !== this.props.allRequiredDoctorInfor) {
-            let { resPayment, resPrice, resProvince, resSpecialty } = this.props.allRequiredDoctorInfor;
+            let { resPayment, resPrice, resProvince, resSpecialty, resClinic } = this.props.allRequiredDoctorInfor;
 
             let dataSelectPrice = this.buildDataInputSelect(resPrice, 'PRICE');
             let dataSelectPayment = this.buildDataInputSelect(resPayment, 'PAYMENT');
             let dataSelectProvince = this.buildDataInputSelect(resProvince, 'PROVINCE');
             let dataSelectSpecialty = this.buildDataInputSelect(resSpecialty, 'SPECIALTY');
+            let dataSelectClinic = this.buildDataInputSelect(resClinic, 'CLINIC');
 
             this.setState({
                 listPrice: dataSelectPrice,
                 listPayment: dataSelectPayment,
                 listProvince: dataSelectProvince,
                 listSpecialty: dataSelectSpecialty,
+                listClinic: dataSelectClinic
             })
         }
 
@@ -170,16 +183,16 @@ class ManageDoctor extends Component {
 
     handleChangeSelect = async (selectedOption) => {
         this.setState({ selectedOption });
-        let { listPayment, listPrice, listProvince, listSpecialty } = this.state;
+        let { listPayment, listPrice, listProvince, listSpecialty, listClinic } = this.state;
 
         let res = await getDetailInforDoctor(selectedOption.value);
         if (res && res.errCode === 0 && res.data && res.data.Markdown) {
             let markdown = res.data.Markdown;
 
             let addressClinic = '', nameClinic = '', note = '',
-                paymentId = '', priceId = '', provinceId = '', specialtyId = '',
+                paymentId = '', priceId = '', provinceId = '', specialtyId = '', clinicId = '',
                 selectedPayment = '', selectedPrice = '', selectedProvince = '',
-                selectedSpecialty = '';
+                selectedSpecialty = '', selectedClinic = '';
 
             if (res.data.Doctor_Infor) {
                 addressClinic = res.data.Doctor_Infor.addressClinic;
@@ -189,6 +202,7 @@ class ManageDoctor extends Component {
                 priceId = res.data.Doctor_Infor.priceId;
                 provinceId = res.data.Doctor_Infor.provinceId;
                 specialtyId = res.data.Doctor_Infor.specialtyId;
+                clinicId = res.data.Doctor_Infor.clinicId;
 
                 selectedPayment = listPayment.find(item => {
                     return item && item.value === paymentId
@@ -201,6 +215,9 @@ class ManageDoctor extends Component {
                 });
                 selectedSpecialty = listSpecialty.find(item => {
                     return item && item.value === specialtyId
+                });
+                selectedClinic = listClinic.find(item => {
+                    return item && item.value === clinicId
                 })
 
             }
@@ -217,6 +234,7 @@ class ManageDoctor extends Component {
                 selectedPrice: selectedPrice,
                 selectedProvince: selectedProvince,
                 selectedSpecialty: selectedSpecialty,
+                selectedClinic: selectedClinic,
             });
         } else {
             this.setState({
@@ -231,6 +249,7 @@ class ManageDoctor extends Component {
                 selectedPrice: '',
                 selectedProvince: '',
                 selectedSpecialty: '',
+                selectedClinic: '',
             });
         }
         console.log(`duypham check res:`, res);
--- src/services/userService.js
+++ src/services/userService.js
@@ -80,9 +80,18 @@ const getAllSpecialty = () => {
     return axios.get(`/api/get-specialty`);
 }
 
+const getAllClinic = () => {
+    return axios.get(`/api/get-clinic`);
+}
+
 const getAllDetailSpecialtyById = (data) => {
     return axios.get(`/api/get-detail-specialty-by-id?id=${data.id}&location=${data.location}`);
 }
+
+const getAllDetailClinicById = (data) => {
+    return axios.get(`/api/get-detail-clinic-by-id?id=${data.id}`);
+}
+
 
 const createNewClinic = (data) => {
     return axios.post('/api/create-new-clinic', data);
@@ -108,5 +117,7 @@ export {
     createNewSpecialty,
     getAllSpecialty,
     getAllDetailSpecialtyById,
-    createNewClinic
+    createNewClinic,
+    getAllClinic,
+    getAllDetailClinicById
 }
